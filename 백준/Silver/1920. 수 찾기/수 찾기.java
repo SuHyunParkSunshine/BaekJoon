57745d58ef9b98cbc12531555c9707531d1806de
SuHyunParkSunshine <129818936+SuHyunParkSunshine@users.noreply.github.com>
2023-09-17 21:26:15 +0900

    [Silver IV] Title: 수 찾기, Time: 1368 ms, Memory: 183016 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,41 +1,69 @@
+// Scanner를 이용하는 방법
+
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int arr[] = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
 
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int[] arrN = new int[N];
-        for (int i = 0; i < arrN.length; i++) {
-            int num = sc.nextInt();
-            arrN[i] = num;
-        }
-
-        int M = sc.nextInt();
-
-        int[] arrM = new int[M];
-        for (int i = 0; i < arrM.length; i++) {
-            int num = sc.nextInt();
-            arrM[i] = num;
-        }
-
-        Set<Integer> setN = new HashSet<>();
-        for (int i = 0; i < N; i++) {
-            setN.add(arrN[i]);
-        }
-
-        for (int i = 0; i < M; i++) {
-            if (setN.contains(arrM[i])) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
-        }
-
-        sc.close();
-    }
+		// 이진탐색 사용시 배열 정리 필요
+		Arrays.sort(arr);
+		
+		int M = sc.nextInt();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < M; i++) {
+			
+			// 찾고자 하는 값이 있을 경우 1, 없을 경우 0 출력
+			if(binarySearch(arr, sc.nextInt()) >= 0) {
+				sb.append(1).append('\n');
+			}
+			else {
+				sb.append(0).append('\n');
+			}
+		}
+		System.out.println(sb);
+		
+	}
+	
+	private static int binarySearch(int arr[], int key) {
+		
+		int low = 0; // 
+		int high = arr.length - 1;
+		
+		//low가 high보다 커질 때까지 반복한다
+		while(low <= high) {
+			
+			int mid = (low + high) / 2; // 중간 위치를 구한다.
+			
+			// key값이 중간위치의 값보다 작은 경우
+			if(key < arr[mid]) {				
+				high = mid - 1;				
+			}
+			
+			// key값이 중간위치의 값보다 큰 경우
+			else if(key > arr[mid]) {				
+				low = mid + 1;				
+			} 
+			
+			else {				
+				return mid;				
+			}
+			
+		}
+		
+		// 찾고자 하는 값이 존재하지 않는 경우
+		return -1;		
+	}
 }
