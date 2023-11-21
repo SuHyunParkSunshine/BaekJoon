02e6855ed5e2b1dcf870697bb1722618cfd31399
SuHyunParkSunshine <129818936+SuHyunParkSunshine@users.noreply.github.com>
2023-11-21 13:47:07 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 140 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,38 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
+
+// ArrayList만 사용하는 version
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> result = new ArrayList<>();
+
+        List<Integer> initialiser = new ArrayList<>();
+        List<Integer> resultContainer = new ArrayList<>();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            queue.add(i + 1);
+        for (int i = 1; i <= N; i++) {
+            initialiser.add(i);
         }
 
-        while (!queue.isEmpty()) {
-            for (int i = 0; i < K - 1; i++) {
-                int pop = queue.poll();
-                queue.add(pop);
-            }
-            int pop = queue.poll();
-            result.add(pop);
+        int i = 0;
+        while (!initialiser.isEmpty()) {
+            // 새로운 idx 계산식
+            int idx = (i + K - 1) % initialiser.size();
+            int result = initialiser.remove(idx);
+            resultContainer.add(result);
+            i = idx;
         }
-        String strResult = result.stream()
+
+        String strResult = resultContainer.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(", ", "<", ">"));
+
         System.out.println(strResult);
     }
 }
